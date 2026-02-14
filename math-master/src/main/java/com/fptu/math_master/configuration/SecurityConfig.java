757b8451d8d19712cd9558c5be1d9f689adbaf75
nhducminhqt <nhducminhqt@gmail.com>
2026-02-15 00:31:01 +0700

    fix: add domain to CORS for webhook

--- math-master/src/main/java/com/fptu/math_master/configuration/SecurityConfig.java
+++ math-master/src/main/java/com/fptu/math_master/configuration/SecurityConfig.java
@@ -77,7 +77,12 @@ public class SecurityConfig {
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000"));
+    configuration.setAllowedOrigins(Arrays.asList(
+      "http://localhost:5173", 
+      "http://localhost:3000",
+      "https://nhducminhqt.name.vn",
+      "http://nhducminhqt.name.vn"
+    ));
     configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
     configuration.setAllowedHeaders(Arrays.asList("*"));
     configuration.setAllowCredentials(true);
