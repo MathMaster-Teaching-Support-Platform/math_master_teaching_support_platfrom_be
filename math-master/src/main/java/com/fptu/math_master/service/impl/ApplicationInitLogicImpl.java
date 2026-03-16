e8e960e69c73efcdad56be6fc0f4c399945adbe2
huypqse <huypqse@gmail.com>
2026-03-16 21:53:00 +0700

    feat: add role guest

--- math-master/src/main/java/com/fptu/math_master/service/impl/ApplicationInitLogicImpl.java
+++ math-master/src/main/java/com/fptu/math_master/service/impl/ApplicationInitLogicImpl.java
@@ -73,6 +73,7 @@ public class ApplicationInitLogicImpl implements ApplicationInitLogic {
     Role studentRole = createRoleIfNotExists(PredefinedRole.STUDENT_ROLE, studentPermissions);
     Role teacherRole = createRoleIfNotExists(PredefinedRole.TEACHER_ROLE, teacherPermissions);
     Role adminRole = createRoleIfNotExists(PredefinedRole.ADMIN_ROLE, allPermissions);
+    Role guestRole = createRoleIfNotExists(PredefinedRole.GUEST_ROLE, new HashSet<>());
 
     log.debug(LOG_CREATING_USERS);
     createUserIfNotExists(properties.getAdmin(), adminRole);
