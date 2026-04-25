c743c7032de0439910df5d91d69d713d80b807e8
dangkhoipham80 <dangkhoipham80@gmail.com>
2026-04-25 16:58:30 +0700

    refactor: reorganize imports in CourseRepository and CourseServiceImpl for clarity

--- math-master/src/main/java/com/fptu/math_master/repository/CourseRepository.java
+++ math-master/src/main/java/com/fptu/math_master/repository/CourseRepository.java
@@ -1,16 +1,18 @@
 package com.fptu.math_master.repository;
 
-import com.fptu.math_master.entity.Course;
-import com.fptu.math_master.enums.CourseStatus;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import com.fptu.math_master.entity.Course;
+import com.fptu.math_master.enums.CourseStatus;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course, UUID> {
